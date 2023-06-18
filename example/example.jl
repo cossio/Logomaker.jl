0646a1b42626ef124495f6f61a1c947f4945152f
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-06-18 18:51:38 +0200

    example

---
+++ example/example.jl
@@ -0,0 +1,32 @@
+#=
+# Logomaker example with RFAM
+=#
+
+using Logomaker: Logo, __example_fasta
+using Statistics: mean
+using LogExpFunctions: xlogx
+import FASTX
+
+# Fetch RNA family alignment RF00162 from RFAM, trimmed by removing insertions.
+fasta_path = __example_fasta()
+records = collect(FASTX.FASTA.Reader(open(fasta_path)))
+seqs = FASTX.sequence.(records)
+
+# RNA nucleotides
+NTs = "ACGU-"
+
+# One-hot representation
+function onehot(s::AbstractString)
+    return reshape(collect(s), 1, length(s)) .== collect(NTs)
+end
+X = reshape(reduce(hcat, onehot.(seqs)), length(NTs), :, length(seqs))
+
+# Compute conservation scores
+p = reshape(mean(X; dims=3), size(X, 1), size(X, 2))
+H = sum(-xlogx.(p) / log(2); dims=1)
+cons = p .* (log2(5) .- H)
+
+# Plot sequence logo!
+logo = Logo(cons, collect(NTs); color_scheme="classic")
+logo.ax.set_ylim(0, log2(5))
+logo.fig
