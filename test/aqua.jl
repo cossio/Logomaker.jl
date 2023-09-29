e160c0eb93d52e14376ef8840cf8994099536f7f
Jorge FdCD <cossio@users.noreply.github.com>
2023-09-29 14:32:43 +0200

    aqua

---
+++ test/aqua.jl
@@ -0,0 +1,8 @@
+import Aqua
+import Logomaker
+
+using Test: @testset
+
+@testset verbose = true "aqua" begin
+    Aqua.test_all(Logomaker)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+module aqua_tests include("aqua.jl") end
+
 using Test: @test
 
 # use doc file as test ... just check that it runs in all platforms tested on CI
