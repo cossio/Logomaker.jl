ab7dbd4466861dc89efeced49d0ba170fb1e2d45
Jorge FdCD <jfdecd@icloud.com>
2024-03-21 17:25:06 +0000

    dev

---
+++ .devcontainer/postCreate.jl
@@ -0,0 +1,3 @@
+import Pkg
+Pkg.activate("/workspaces/Logomaker.jl")
+Pkg.update()
--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,1 +1,4 @@
 gh auth login -h github.com --with-token < /home/vscode/secrets/gh_token.txt
+
+# Update workspace environment
+/home/vscode/.juliaup/bin/julia .devcontainer/postCreate.jl
